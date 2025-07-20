c3217b40e9d76511948f13d7bf416e8e9238cbcf
MD.MRIDUL <mridulaiub17@gmail.com>
2025-07-20 15:00:05 +0600

    added form structure

---
+++ view/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<body>
+    <center>
+        AIUB
+        This is registration form
+        
+    </center>
+   <table>
+    <tr>
+        <td>
+            
+        </td>
+    </tr>
+   </table>
+</body>
+</html>
